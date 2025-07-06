f3ae8dc5c91fdc6ac0b88e1152879ecbff1dbbda
Pingu <106933377+pingu6@users.noreply.github.com>
2025-07-06 22:54:47 +0400

    fix: incorrect column data type

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS
     prefixes (
-        guild_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        prefix String
+        guild_id INTEGER PRIMARY KEY,
+        prefix TEXT
     );
